8cb25de30964579e13a58fc4cae6402a83f3dc51
sebastian.soderholm <sebastian.soderholm@fi.experis.com>
2021-08-26 17:17:01 +0300

    Added Program.cs

---
+++ Assignment2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Assignment2.Exceptions;
+using Assignment2.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerRepository customerRepo = new CustomerRepository(@"5CG05206QS\SQLEXPRESS", "Chinook");
+            //Console.WriteLine(customerRepo.GetCustomerById(45));
+
+            try
+            {
+                List<Customer> customerPage = customerRepo.GetCustomersPage(2,20);
+
+                foreach(Customer customer in customerPage)
+                {
+                    Console.WriteLine(customer.ToString());
+                }
+            }
+            catch (RepositoryException re)
+            {
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
