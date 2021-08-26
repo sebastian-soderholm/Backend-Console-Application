bf3f4e287dd8a075882d9d84e32aaaf535a6b2ff
Peppi <peppi.makela@gmail.com>
2021-08-26 17:52:11 +0300

    Changed Program

---
+++ Assignment2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Assignment2.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomerRepository customerRepo = new CustomerRepository(@"5CG05206QV\SQLEXPRESS", "Chinook");
+            //Console.WriteLine(customerRepo.GetHighestSpendingCustomers());
+            
+            Console.WriteLine(customerRepo.GetCustomerByName("Hannah"));
+            //Console.WriteLine(customerRepo.GetCustomerById(42));
+
+            foreach (CustomerSpender i in customerRepo.GetHighestSpendingCustomers())
+            {
+                Console.WriteLine(i);
+            }
+
+
+            //Customer testCustomer = new Customer()
+            //{
+            //    FirstName = "Bruce",
+            //    LastName = "Wayne",
+            //    Country = "USA",
+            //    PostalCode = "53540",
+            //    PhoneNumber = "050123456",
+            //    Email = "batman@wayneenterprises.com"
+            //};
+            //customerRepo.AddCustomer(testCustomer);
+            /*
+            testCustomer = customerRepo.GetCustomerById(1);
+            testCustomer.FirstName = "Luis";
+            customerRepo.UpdateCustomer(testCustomer);
+            List<CustomerCountry> testList = customerRepo.GetNumberOfCustomersByCountry();
+            //LinQ magics
+            testList.ForEach(x => Console.WriteLine(x.Name + " | " + x.Count));
+            */
+        }
+    }
+}
